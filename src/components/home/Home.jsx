857ff3a8ed89fd86c1c4dfbce3c477538b07545a
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-11 22:47:50 +0600

    Axios custom hook created to get data

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,24 @@
-import { useLoaderData } from 'react-router-dom';
 import Brand from '../../brand/Brand';
 import Banner from '../../banner/Banner';
 import AboutUs from '../aboutUs/AboutUs';
 import ContactUs from '../contactUs/ContactUs';
+import useAxios from './../../hooks/useAxios';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
-  const brands = useLoaderData();
+  const axios = useAxios();
+  const [brands, setBrands] = useState([]);
+  useEffect(() => {
+    axios.get('/brands').then(res => {
+      setBrands(res.data);
+    });
+  }, [axios]);
   return (
     <div className="">
       <Banner></Banner>
       <h1 className="text-center text-4xl font-bold my-5">Worlds top brands</h1>
       <div className=" max-w-[1480px] mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {brands.map(brand => (
+        {brands?.map(brand => (
           <Brand key={brand.id} brand={brand}></Brand>
         ))}
       </div>
