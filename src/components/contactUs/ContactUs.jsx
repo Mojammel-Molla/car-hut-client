e007005d89d910b0bde431b805f3bb3af4e13fb7
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-11 23:59:17 +0600

    Contact us page style update

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -4,18 +4,18 @@ const ContactUs = () => {
   return (
     <div>
       <h1 className="text-4xl text-center font-bold my-10">Contact Us</h1>
-      <section className="grid mt-5 bg-slate-200 rounded-md  gap-3 lg:grid-cols-3  sm:grid-cols-1">
-        <div className="p-20  text-center  mx-auto">
+      <section className="grid mt-5 gap-3 lg:grid-cols-3  sm:grid-cols-1">
+        <div className="lg:p-20 shadow-2xl text-center  mx-auto">
           <GrMailOption className="text-2xl ml-20"></GrMailOption>
 
           <h2 className="font-semibold lg:ml-6">info@carhut.com</h2>
         </div>
-        <div className="lg:p-20  text-center  mx-auto">
+        <div className="lg:p-20 shadow-2xl  text-center  mx-auto">
           <MdLocationOn className="text-2xl ml-20"></MdLocationOn>
           <h2 className="font-semibold">Dhaka</h2>
           <p className="font-light">kuril chowrasta, badda, 1212</p>
         </div>
-        <div className="p-20  text-center  mx-auto">
+        <div className="lg:p-20 shadow-2xl   text-center  mx-auto">
           <GrPhone className=" ml-10 text-2xl"></GrPhone>
 
           <h2 className="font-semibold">+1300 138 892</h2>
