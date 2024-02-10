9c559010fc31d7227d3c1bbc4071a3ba080df823
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-02-10 23:54:49 +0600

    Banner section added

--- src/banner/Banner.jsx
+++ src/banner/Banner.jsx
@@ -1,24 +1,22 @@
-import bannerImage from '../assets/34861221_8218093.jpg';
+// import bannerImage from '../assets/34861221_8218093.jpg';
+import BannerImage from '../assets/car-banner (2).jpg';
 const Banner = () => {
   return (
     <>
-      <div
-        className="hero w-full  min-h-screen bg-cover mx-auto mt-5"
-        style={{
-          backgroundImage: `url(${bannerImage})`,
-        }}
-      >
-        <div className=" "></div>
-        <div className="hero-content text-center text-neutral-content">
-          {/* <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div> */}
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={BannerImage} className="max-w-2xl rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Buy Your Dream Car Today!</h1>
+            <p className="py-6">
+              Welcome to Car Hut, your premier destination for buying and
+              selling high-quality new and used cars in Bangladesh. Whether you
+              are in the market for a sleek sedan, a rugged SUV, or a sporty
+              coupe, we have an extensive inventory to suit every taste and
+              budget.
+            </p>
+            <button className="btn bg-orange-900 text-white">Buy Now</button>
+          </div>
         </div>
       </div>
     </>
