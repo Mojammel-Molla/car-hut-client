c0e18abbf6fd48e778e64d66de6226ad3fb79b7d
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-02-11 00:00:10 +0600

    Contact page updated

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -4,7 +4,7 @@ const ContactUs = () => {
   return (
     <div>
       <h1 className="text-4xl text-center font-bold my-10">Contact Us</h1>
-      <section className="grid mt-5 gap-3 lg:grid-cols-3  sm:grid-cols-1">
+      <section className="grid mt-5 gap-3 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
         <div className="lg:p-20 shadow-2xl text-center rounded-2xl mx-auto">
           <GrMailOption className="text-2xl ml-20"></GrMailOption>
 
@@ -14,6 +14,13 @@ const ContactUs = () => {
           <MdLocationOn className="text-2xl ml-20"></MdLocationOn>
           <h2 className="font-semibold">Dhaka</h2>
           <p className="font-light">kuril chowrasta, badda, 1212</p>
+        </div>
+        <div className="lg:p-20 shadow-2xl rounded-2xl text-center  mx-auto">
+          <GrPhone className=" ml-10 text-2xl"></GrPhone>
+
+          <h2 className="font-semibold">+1300 138 892</h2>
+          <p className="font-light">Monday - Saturday</p>
+          <p className="font-light">9 AM - 6 PM</p>
         </div>
         <div className="lg:p-20 shadow-2xl rounded-2xl text-center  mx-auto">
           <GrPhone className=" ml-10 text-2xl"></GrPhone>
