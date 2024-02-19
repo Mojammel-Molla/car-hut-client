6d0efc3b39b95c032107e8bfc4187a95d8370b13
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-02-19 23:57:21 +0600

    Blog page data dynamically added in to the blogs page

---
+++ src/components/blogs/Blog.jsx
@@ -0,0 +1,45 @@
+const Blog = ({ blog, isShow, setIsShow }) => {
+  const {
+    blog_title,
+    blog_description,
+    blog_image,
+    publish_date,
+    writer_name,
+    writer_image,
+  } = blog || {};
+  return (
+    <div className="hero min-h-[80vh] bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={blog_image} className="max-w-lg rounded-lg shadow-2xl" />
+        <div className="pt-10">
+          <h1 className="text-3xl font-bold">{blog_title}</h1>
+          {isShow ? (
+            <p className="py-6">{blog_description}</p>
+          ) : (
+            <p className="py-6">{blog_description.slice(0, 200)}</p>
+          )}
+          <button onClick={() => setIsShow(!isShow)} className="mb-5 p-5">
+            {isShow ? 'See Less...' : 'See More...'}
+          </button>
+          <div className="flex justify-between">
+            <div>
+              <img
+                className="h-10 w-10 rounded-full ml-4"
+                src={writer_image}
+                alt="Car Image"
+              />
+              <p>
+                Writer : <span className="font-bold">{writer_name}</span>
+              </p>
+            </div>
+            <p>
+              Date: <span className="font-bold">{publish_date}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,21 +1,27 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Blog from './Blog';
+
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    fetch('http://localhost:5000/blogs')
+      .then(res => res.json())
+      .then(data => setBlogs(data));
+  }, []);
+  console.log(blogs, isShow);
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
-      </div>
+    <div>
+      {blogs.map(blog => (
+        <Blog
+          key={blog._id}
+          blog={blog}
+          isShow={isShow}
+          setIsShow={setIsShow}
+        ></Blog>
+      ))}
+    </div>
   );
 };
 
