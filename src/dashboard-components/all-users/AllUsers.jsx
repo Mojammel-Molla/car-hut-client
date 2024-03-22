c9074a62093931f55d8664d40f2bc8418433e752
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-03-22 16:31:05 +0600

    User delete method created

--- src/dashboard-components/all-users/AllUsers.jsx
+++ src/dashboard-components/all-users/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import SectionBanner from '../../shared/section-banner/SectionBanner';
 import useAxios from '../../hooks/useAxios';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
   const axios = useAxios();
@@ -9,6 +10,30 @@ const AllUsers = () => {
     axios.get('/users').then(res => setUsers(res.data));
   }, [axios]);
   console.log(users);
+
+  const handleDelete = id => {
+    // console.log(id);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        axios.delete(`/users/${id}`).then(res => {
+          res.data;
+
+          console.log(res.data);
+          if (res.data.deletedCount > 0) {
+            Swal.fire('Deleted!', 'User has been deleted.', 'success');
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <SectionBanner title="Dashboard" subTitle="All Users"></SectionBanner>
@@ -32,7 +57,10 @@ const AllUsers = () => {
                 <td>{user.email}</td>
                 <td>{user.role}</td>
                 <td>
-                  <button className=" bg-red-600 text-white font-medium p-1 rounded-md hover:bg-slate-600">
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className=" bg-red-600 text-white font-medium p-1 rounded-md hover:bg-slate-600"
+                  >
                     Delete
                   </button>
                 </td>
